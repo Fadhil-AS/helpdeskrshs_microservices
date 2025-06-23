c013b00f369fd93358b4f4645446450a5c05542e
Faris Siddiq <farissiddiqrp@gmail.com>
2025-06-23 18:30:13 +0700

    Menambahkan tampilan logout di role unit kerja

--- resources/views/Services/UnitKerja/partials/navbarUnitKerja.blade.php
+++ resources/views/Services/UnitKerja/partials/navbarUnitKerja.blade.php
@@ -7,13 +7,21 @@
             <img src="{{ asset('assets/images/logoRSHS.png') }}" alt="Logo" height="40" />
         </a>
 
-        <div class="d-flex gap-2">
-            <button class="btn btn-link" type="button">
-                <i class="bi bi-bell offcanvas-icons"></i>
-            </button>
-            <button class="btn btn-link" type="button">
-                <i class="bi bi-person offcanvas-icons"></i>
-            </button>
+        <div class="dropdown">
+            <a href="#" class="profile-dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end mt-2">
+                <li><h6 class="dropdown-header">Selamat Datang,</h6></li>
+                <li><p class="dropdown-item-text px-3">{{ Auth::user()->name ?? 'Nama Pengguna' }}</p></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <a href="#" class="dropdown-item">
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
